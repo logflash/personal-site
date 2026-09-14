b14c3941b7259348e440f6d39a474f3894e0f0b8
Ian Henriques <ih2422@princeton.edu>
2026-09-14 04:05:24 -0400

    Add glyphflux as submodule

--- repos/glyphflux/bin/glyphflux.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import { runGlyphfluxCli } from '../dist/cli.mjs'
-
-process.exitCode = await runGlyphfluxCli()
--- repos/glyphflux/src/fontkit.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'fontkit' {
-  export function create(buffer: Uint8Array, postscriptName?: string | null): unknown
-}
